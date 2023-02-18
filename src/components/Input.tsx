fb0db22e51264b6202983ac9de5b83405da8a82b
amrendranath <cool.amrendrasrivastav@gmail.com>
2023-02-18 19:27:45 +0530

    remove comment

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,21 +1,13 @@
 import React from "react";
 import { TextField, InputLabel, TextFieldProps } from "@mui/material";
-// import { useController, UseControllerProps } from "react-hook-form";
-
-// type FormValues = {
-//   firstName: string;
-//   lastName: string;
-// };
 
 type InputProps = {
   label: string;
   id: string;
 } & TextFieldProps;
-// & UseControllerProps<FormValues>;
 
 const Input = React.forwardRef((props: InputProps, ref) => {
   const { label, id, name, ...rest } = props;
-  // const { field } = useController({ name, control });
 
   return (
     <>
