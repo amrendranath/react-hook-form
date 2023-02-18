be53e1156f3a189b2628b37c0cc50c6b42c292d3
amrendranath <cool.amrendrasrivastav@gmail.com>
2023-02-18 19:24:55 +0530

    remove app.test.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,11 @@
-import "./index.css";
-import { useForm } from "react-hook-form";
-import { Button } from "@mui/material";
-import Form from "./Form";
+import UseControllerForm from "./useControllerForm/UseControllerForm";
 
 type AppProps = {
   login: (email: string, password: string) => Promise<unknown>;
 };
 
-type FormValues = {
-  email: string;
-  password: string;
+const App = ({ login }: AppProps) => {
+  return <UseControllerForm login={login} />;
 };
 
-export default function App({ login }: AppProps) {
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm<FormValues>({
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
-
-  const onSubmit = async (data: FormValues) => {
-    await login(data.email, data.password);
-    reset();
-  };
-
-  // console.log(errors);
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <h1>Login</h1>
-      <Form control={control} errors={errors} />
-      <Button type="submit">SUBMIT</Button>
-    </form>
-  );
-}
+export default App;
---
+++ src/useControllerForm/UseControllerForm.tsx
@@ -0,0 +1,82 @@
+import "../index.css";
+import { useForm, Controller } from "react-hook-form";
+import { Button } from "@mui/material";
+import Input from "../components/Input";
+
+type UseControllerFormProps = {
+  login: (email: string, password: string) => Promise<unknown>;
+};
+
+type FormValues = {
+  email: string;
+  password: string;
+};
+
+export default function UseControllerForm({ login }: UseControllerFormProps) {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: FormValues) => {
+    await login(data.email, data.password);
+    reset();
+  };
+
+  // console.log(errors);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1>Login</h1>
+      <Controller
+        name="email"
+        control={control}
+        render={({ field }) => (
+          <Input
+            {...field}
+            id="email"
+            label="Email"
+            error={!!errors?.email}
+            helperText={errors?.email?.message}
+          />
+        )}
+        rules={{
+          required: "Email is required",
+          pattern: {
+            value: /\S+@\S+\.\S+/,
+            message: "Entered value does not match email format",
+          },
+        }}
+      />
+      <Controller
+        name="password"
+        control={control}
+        render={({ field }) => (
+          <Input
+            {...field}
+            id="password"
+            label="Password"
+            type="password"
+            error={!!errors?.password}
+            helperText={errors?.password?.message}
+          />
+        )}
+        rules={{
+          required: "Password is required",
+          minLength: {
+            value: 5,
+            message: "Password must be greater than 5 character",
+          },
+        }}
+      />
+      <Button type="submit">SUBMIT</Button>
+    </form>
+  );
+}
